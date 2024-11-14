6e9839dce9d92e82feb191c99a99c38e214564e4
DreamingShynnevilla <158278770+PhamMinhTrietHCMUS@users.noreply.github.com>
2024-11-14 22:19:52 +0700

    Revert "added constructor and destructor for coin"
    This reverts commit 41556646407f0fc051e875a94da8c68854b95f73.

--- Coin.cpp
+++ Coin.cpp
@@ -1,18 +1,1 @@
 #include "Coin.h"
-
-Coin::Coin(sf::Vector2f position)
-{
-    // Assuming Resources is a class that provides access to textures
-    const sf::Texture& texture = Resources::textures["coin_texture"];
-    int totalFrames = 6; // Example value, set according to your sprite sheet
-    float frameDuration = 0.1f; // Example value, set according to your animation speed
-    sf::Vector2i frameSize(32, 32); // Example value, set according to your sprite sheet
-
-    this->animation = new Animation(texture, totalFrames, frameDuration, frameSize);
-    this->animation->setPosition(position);
-}
-
-Coin::~Coin()
-{
-    delete this->animation;
-}
