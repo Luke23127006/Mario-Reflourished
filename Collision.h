67ad9c644c7d3b85282bdd31a64d701d084943a3
Luke23127006 <tnkluan23@clc.fitus.edu.vn>
2024-12-16 14:18:58 +0700

    clean the class collision

--- Collision.h
+++ Collision.h
@@ -13,10 +13,6 @@ public:
 	static void handle_entity_map(Entity* entity, Map* map);
 	static void handle_entity_tile(Entity* entity, Tile* tile);
 	static void handle_entity_entity(Entity* entity, Entity* other);
-	static void handle_player_enemy(Player* player, Enemy* enemy);
-	static void handle_entity_shell(Entity* entity, Shell* shell);
-	static void handle_bullet_enemy(Bullet* bullet, Enemy* enemy);
-	static void handle_player_powerUp(Player* player, PowerUp* powerUp);
 	static void handle_entity_spikeWall(Entity* entity, SpikeWall* spikeWall);
 	static void handle_player_coin(Player* player, Coin* coin);
 };
